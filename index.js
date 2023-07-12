c2596eb65a8fdc6e66fa93f9a630f6615606c9d0
Hossein <h.moghtafari@hotmail.com>
2023-07-12 23:18:34 +0400

    changing logic of the button

--- index.js
+++ index.js
@@ -1,76 +1,96 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import {
-  getDatabase,
-  ref,
-  push,
-  onValue,
-  remove,
-} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import {initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-  databaseURL: "https://publish-endorsement-default-rtdb.firebaseio.com/",
-};
+    databaseURL: "https://publish-endorsement-default-rtdb.firebaseio.com/"
+}
 
-const app = initializeApp(appSettings);
-const database = getDatabase(app);
-const endorsementListInDB = ref(database, "endorsementList");
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const endorsementListInDB = ref (database, "endorsementList")
 
-const inputEl = document.getElementById("input-txt");
-const publishBtn = document.getElementById("publish-btn");
-const endorsementListEl = document.getElementById("endorsement-el");
 
-publishBtn.addEventListener("click", function () {
-  // let inputEldata = inputEl.value
-  let inputEldata = inputEl.value.trim(); // Trim any whitespace
+const inputEl = document.getElementById("input-txt")
+const publishBtn = document.getElementById("publish-btn")
+const endorsementListEl = document.getElementById("endorsement-el")
 
-  if (inputEldata === "") {
-    // Display error message
-    alert("The box should not be empty");
-    return; // Stop further execution
-  }
-  // console.log(inputEldata)
-  push(endorsementListInDB, inputEldata);
-  // endorsementListEl.innerHTML += `<li>${inputEldata}</li>`
-  clearInputEl();
-  // console.log(endorsementListInDB)
-});
-
-onValue(endorsementListInDB, function (snapshot) {
-  if (snapshot.exists()) {
-    let itemArray = Object.entries(snapshot.val());
-    let reversedArray = itemArray.reverse(); // Reverse the array
-
-    // console.log(itemArrey)
-
-    removeEndorsementList();
-
-    for (let i = 0; i < reversedArray.length; i++) {
-      let currentItems = reversedArray[i];
-      // let currentItemsID = currentItems[0];
-      // let currentItemsValue = currentItems[1];
-
-      appendItemsToEndorsementList(currentItems);
+publishBtn.addEventListener("click", function(){
+    // let inputEldata = inputEl.value 
+    let inputEldata = inputEl.value.trim(); // Trim any whitespace
+    
+    if (inputEldata === "") {
+        // Display error message
+        alert("The box should not be empty");
+        return; // Stop further execution
     }
-  } else {
-    endorsementListEl.innerHTML = "No items here... yet";
-  }
-});
-function removeEndorsementList() {
-  endorsementListEl.innerHTML = "";
+    // console.log(inputEldata)
+    push(endorsementListInDB, inputEldata)
+    // endorsementListEl.innerHTML += `<li>${inputEldata}</li>`
+    clearInputEl()
+    // console.log(endorsementListInDB)
+})
+//trying with another way//
+// publishBtn.addEventListener("click", function () {
+//     // let inputEldata = inputEl.value
+//     let inputEldata =! inputEl.value.trim(); // Trim any whitespace
+  
+//     if (inputEldata) {
+//       publishBtn.disabled = false
+//       // Display error message
+//       // alert("The box should not be empty");
+//       return; // Stop further execution
+//     } 
+//   //   else{
+  
+//   //       publishBtn.disabled = false
+//   //   }
+//     // console.log(inputEldata)
+//     push(endorsementListInDB, inputEldata);
+//     // endorsementListEl.innerHTML += `<li>${inputEldata}</li>`
+//     clearInputEl();
+//     // console.log(endorsementListInDB)
+//   });
+//trying with another way//
+
+onValue(endorsementListInDB, function(snapshot){
+    if (snapshot.exists()){
+        let itemArray = Object.entries(snapshot.val())
+        let reversedArray = itemArray.reverse(); // Reverse the array
+
+        // console.log(itemArrey)
+        
+    removeEndorsementList()
+    
+    for(let i = 0 ; i < reversedArray.length ; i++) {
+        let currentItems = reversedArray[i]
+        let currentItemsID = currentItems[0]
+        let currentItemsValue = currentItems[1]
+        
+        appendItemsToEndorsementList(currentItems)
+    }
+    } else {
+        endorsementListEl.innerHTML = "No items here... yet"
+    }
+})
+function removeEndorsementList(){
+    endorsementListEl.innerHTML = ""
 }
-function clearInputEl() {
-  inputEl.value = "";
+function clearInputEl(){
+    inputEl.value = ""
 }
 
-function appendItemsToEndorsementList(item) {
-  let itemId = item[0];
-  let itemValue = item[1];
+function appendItemsToEndorsementList(item){
+    let itemId = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li") 
+    newEl.textContent = itemValue
 
-  let newEl = document.createElement("li");
-  newEl.textContent = itemValue;
-  newEl.addEventListener("dblclick", function () {
-    let exactLocationOfItemInDB = ref(database, `endorsementList/${itemId}`);
-    remove(exactLocationOfItemInDB);
-  });
-  endorsementListEl.append(newEl);
+    newEl.addEventListener("dblclick", function(){
+        let exactLocationOfItemInDB = ref(database, `endorsementList/${itemId}`)
+remove(exactLocationOfItemInDB)
+
+    })
+    endorsementListEl.append(newEl)
+    
 }
